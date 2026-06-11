93aa3a514f401357cb29e2b75feb5eb1b222a9ec
Atul <atulprjpt@gmail.com>
2026-06-11 20:07:56 +0530

    map issue fixed

--- public/javascripts/ShowPageMap.js
+++ public/javascripts/ShowPageMap.js
@@ -1,19 +1,25 @@
 
-mapboxgl.accessToken = mapToken;
-const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v12', // style URL
-    center: campground.geometry.coordinates, // starting position [lng, lat]
-    zoom: 9, // starting zoom
-});
+const coordinates = campground.geometry && campground.geometry.coordinates;
 
-const nav = new mapboxgl.NavigationControl();
-map.addControl(nav, 'top-left');
+if (coordinates) {
+    mapboxgl.accessToken = mapToken;
+    const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: 'mapbox://styles/mapbox/streets-v12', // style URL
+        center: coordinates, // starting position [lng, lat]
+        zoom: 9, // starting zoom
+    });
 
-// Create a Marker and add it to the map.
-const marker1 = new mapboxgl.Marker()
-    .setLngLat(campground.geometry.coordinates)
-    .setPopup(new mapboxgl
-        .Popup()
-        .setHTML(`<h3>${campground.title}</h3><p>${campground.location}</p>`)) // add popup
-    .addTo(map);
+    const nav = new mapboxgl.NavigationControl();
+    map.addControl(nav, 'top-left');
+
+    // Create a Marker and add it to the map.
+    new mapboxgl.Marker()
+        .setLngLat(coordinates)
+        .setPopup(new mapboxgl
+            .Popup()
+            .setHTML(`<h3>${campground.title}</h3><p>${campground.location}</p>`)) // add popup
+        .addTo(map);
+} else {
+    console.warn('Campground is missing geometry coordinates.');
+}
